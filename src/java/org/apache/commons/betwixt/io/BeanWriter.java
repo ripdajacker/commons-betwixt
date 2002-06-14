d84bf9464530af0b34b6169a149d24df151e3d41
Martin van den Bemt <mvdb@apache.org>
2002-06-14 23:05:26 +0000

    Forgot to finish up comment..
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/betwixt/trunk@128881 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/betwixt/io/BeanWriter.java
+++ src/java/org/apache/commons/betwixt/io/BeanWriter.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//betwixt/src/java/org/apache/commons/betwixt/io/BeanWriter.java,v 1.4 2002/06/14 23:00:15 mvdb Exp $
- * $Revision: 1.4 $
- * $Date: 2002/06/14 23:00:15 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//betwixt/src/java/org/apache/commons/betwixt/io/BeanWriter.java,v 1.5 2002/06/14 23:05:26 mvdb Exp $
+ * $Revision: 1.5 $
+ * $Date: 2002/06/14 23:05:26 $
  *
  * ====================================================================
  *
@@ -57,7 +57,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  * 
- * $Id: BeanWriter.java,v 1.4 2002/06/14 23:00:15 mvdb Exp $
+ * $Id: BeanWriter.java,v 1.5 2002/06/14 23:05:26 mvdb Exp $
  */
 package org.apache.commons.betwixt.io;
 
@@ -127,7 +127,7 @@ import org.apache.commons.betwixt.io.id.SequentialIDGenerator;
   * 
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
   * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
-  * @version $Revision: 1.4 $
+  * @version $Revision: 1.5 $
   */
 public class BeanWriter {
 
@@ -146,7 +146,7 @@ public class BeanWriter {
     private XMLIntrospector introspector = new XMLIntrospector();
     /** Where the output goes */
     private Writer writer;    
-    /** text used for end of lines. Defaults to  */
+    /** text used for end of lines. Defaults to <code>\n</code>*/
     private String endOfLine = "\n";
     /** indentation text */
     private String indent;
