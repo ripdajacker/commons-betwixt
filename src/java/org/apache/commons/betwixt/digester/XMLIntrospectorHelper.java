4819a9851354349648a686b022c733551e51c7be
Martin van den Bemt <mvdb@apache.org>
2003-02-03 14:23:31 +0000

    Fixing nullpointer exception scenario when debugging.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/betwixt/trunk@129082 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/betwixt/digester/XMLIntrospectorHelper.java
+++ src/java/org/apache/commons/betwixt/digester/XMLIntrospectorHelper.java
@@ -84,7 +84,7 @@ import org.apache.commons.logging.LogFactory;
   *
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
   * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
-  * @version $Id: XMLIntrospectorHelper.java,v 1.16 2003/01/08 22:07:21 rdonkin Exp $
+  * @version $Id: XMLIntrospectorHelper.java,v 1.17 2003/02/03 14:23:31 mvdb Exp $
   */
 public class XMLIntrospectorHelper {
 
@@ -392,7 +392,8 @@ public class XMLIntrospectorHelper {
 
                         if ( log.isDebugEnabled() ) {
                             log.debug( "!! " + propertyName + " -> " + descriptor );
-                            log.debug( "!! " + name + " -> " + descriptor.getPropertyName() );
+                            log.debug( "!! " + name + " -> " + 
+                            (descriptor!=null?descriptor.getPropertyName():"") );
                         }
                         
                         if ( descriptor != null ) {
