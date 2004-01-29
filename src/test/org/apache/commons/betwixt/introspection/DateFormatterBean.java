83813d846c215dfbe0f617c8ccae0d89e50b3cf4
Robert Burrell Donkin <rdonkin@apache.org>
2004-01-29 22:14:55 +0000

    Test bean for refactored map description code.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/betwixt/branches/REFACTORING-BRANCH_2004-01-13@129263 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/betwixt/introspection/DateFormatterBean.java
@@ -0,0 +1,107 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//betwixt/src/test/org/apache/commons/betwixt/introspection/DateFormatterBean.java,v 1.1.2.1 2004/01/29 22:14:55 rdonkin Exp $
+ * $Revision: 1.1.2.1 $
+ * $Date: 2004/01/29 22:14:55 $
+ *
+ * ====================================================================
+ * 
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2004 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgement:  
+ *       "This product includes software developed by the 
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
+ *
+ * 4. The names "Apache", "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior 
+ *    written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */ 
+
+package org.apache.commons.betwixt.introspection;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author <a href='http://jakarta.apache.org/'>Jakarta Commons Team</a>
+ * @version $Revision: 1.1.2.1 $
+ */
+public class DateFormatterBean {
+    
+    private Map formatsByLocale = new HashMap();
+    
+    //TODO: want to be able to match iterators from maps...
+    //      but this mean ammending the descriptors when the property type
+    //      is discovered
+    public Map getFormats() {
+        return formatsByLocale;
+    }
+    
+    //TODO: should be able to use another verb eg register
+    //TODO: support for specifying adders in the dot betwixt file
+    //TODO: support for Simple types so that we can use Locale -> String
+    public void addFormat(Locale locale, SimpleDateFormat format) {
+        formatsByLocale.put(locale, format);
+    }
+    
+    public String format(Date date, Locale locale) {
+        String result = "";
+        SimpleDateFormat simpleDateFormat = (SimpleDateFormat) formatsByLocale.get(locale);
+        if (simpleDateFormat == null)
+        {
+             result = DateFormat.getDateInstance(DateFormat.SHORT, locale).format(date);
+        }
+        else
+        {
+            result = simpleDateFormat.format(date);
+        }
+        return result;
+    }
+    
+}
