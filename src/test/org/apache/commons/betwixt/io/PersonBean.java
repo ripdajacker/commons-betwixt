7da7f5be876784a71046fc76c4a2899ab8deb6b0
Robert Burrell Donkin <rdonkin@apache.org>
2004-03-31 19:55:59 +0000

    Tests for modified writing API
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/betwixt/trunk@129308 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/betwixt/io/PersonBean.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.commons.betwixt.io;
+
+/**
+ * @author <a href='http://jakarta.apache.org/'>Jakarta Commons Team</a>
+ * @version $Revision: 1.1 $
+ */
+public class PersonBean {
+    private String forenames;
+    private String surname;
+    
+    public PersonBean() {}
+    
+    public PersonBean(String forenames, String surname) {
+        setSurname(surname);
+        setForenames(forenames);
+    }
+
+    public String getForenames() {
+        return forenames;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setForenames(String string) {
+        forenames = string;
+    }
+
+    public void setSurname(String string) {
+        surname = string;
+    }
+
+}
