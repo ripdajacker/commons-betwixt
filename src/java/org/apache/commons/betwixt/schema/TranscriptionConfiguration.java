186c67d938e81edfd785263b97048dd5066bd388
No Author <dev-null@apache.org>
2004-06-13 21:32:49 +0000

    This commit was manufactured by cvs2svn to create tag 'REFACTORING-BRANCH_2004-01-13_MERGED'.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/betwixt/tags/REFACTORING-BRANCH_2004-01-13_MERGED@129354 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/betwixt/schema/TranscriptionConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
+
+package org.apache.commons.betwixt.schema;
+
+/**
+ * Configuration for XMLBeanInfo to XML schema transcription.
+ * All settings are gathered into this one class for convenience.
+ * 
+ * @author <a href='http://jakarta.apache.org/'>Jakarta Commons Team</a>
+ * @version $Revision: 1.1.2.2 $
+ */
+public class TranscriptionConfiguration {
+    
+    private DataTypeMapper dataTypeMapper = new DefaultDataTypeMapper();
+    
+    
+    /**
+     * Gets the <code>DataTypeMapper</code> to be used during the transcription.
+     * @return DataTypeMapper, not null
+     */
+    public DataTypeMapper getDataTypeMapper() {
+        return dataTypeMapper;
+    }
+
+    /**
+     * Sets the <code>DataTypeMapper</code> to be used during the transcription/
+     * @param mapper DataTypeMapper, not null
+     */
+    public void setDataTypeMapper(DataTypeMapper mapper) {
+        dataTypeMapper = mapper;
+    }
+
+}
