c9afaafae282549966f76da17a80dd6c6448ad64
Jesenko Mehmedbasic <jekm@r-m.com>
2015-05-21 20:07:27 +0200

    Gitignore

---
+++ src/java/dk/mehmedbasic/betwixt/EnhancedBeanInfo.java
@@ -0,0 +1,10 @@
+package dk.mehmedbasic.betwixt;
+
+/**
+ * TODO - someone remind me to document this class
+ *
+ * @author Jesenko Mehmedbasic
+ *         created 5/19/2015.
+ */
+public class EnhancedBeanInfo {
+}
