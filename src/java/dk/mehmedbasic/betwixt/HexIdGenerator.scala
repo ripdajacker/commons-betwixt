964581adc15272fe10a6e997fc4bca9135baf688
Jesenko Mehmedbasic <jekm@r-m.com>
2015-09-17 21:52:19 +0200

    HexIdGenerator

--- src/java/dk/mehmedbasic/betwixt/HexIdGenerator.scala
+++ src/java/dk/mehmedbasic/betwixt/HexIdGenerator.scala
@@ -3,16 +3,13 @@ package dk.mehmedbasic.betwixt
 import org.apache.commons.betwixt.io.id.AbstractIDGenerator
 
 /**
- * TODO - someone remind me to document this class 
- *
- * @author Jesenko Mehmedbasic
- *         created 6/10/2015.
+ * An id generator that generates hexidecimal strings.
  */
 class HexIdGenerator extends AbstractIDGenerator {
-  private var counter = 0
+   private var counter = 0
 
-  override protected def nextIdImpl(): String = {
-    counter += 1
-    Integer.toHexString(counter)
-  }
+   override protected def nextIdImpl(): String = {
+      counter += 1
+      Integer.toHexString(counter)
+   }
 }
