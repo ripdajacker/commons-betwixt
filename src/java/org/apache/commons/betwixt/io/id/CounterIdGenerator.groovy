2dc99e13bcfb547423b8456bd4227f20486c29a5
Jesenko Mehmedbasic <jekm@r-m.com>
2016-02-02 00:27:16 +0100

    Refactored id strategies

--- src/java/org/apache/commons/betwixt/io/id/CounterIdGenerator.groovy
+++ src/java/org/apache/commons/betwixt/io/id/CounterIdGenerator.groovy
@@ -1,18 +1,24 @@
 package org.apache.commons.betwixt.io.id
 
 import groovy.transform.TypeChecked
+import org.apache.commons.betwixt.io.IDGenerator
 
 /**
  * A generator with a counter
  */
 @TypeChecked
-abstract class CounterIdGenerator extends AbstractIDGenerator {
+abstract class CounterIdGenerator implements IDGenerator {
     private int counter = 0
 
     @Override
-    protected String nextIdImpl() {
+    String nextId() {
         return convert(++counter)
     }
 
     abstract String convert(int counter)
+
+    @Override
+    String getLastId() {
+        return null
+    }
 }
